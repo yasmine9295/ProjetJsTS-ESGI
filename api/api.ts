75c339730b978c642f47e33b97ab0680737b2bac
yasmine9295 <yasmine.elharim92@gmail.com>
2026-03-10 14:06:19 +0100

    api2

---
+++ api/api.ts
@@ -0,0 +1,32 @@
+interface price{
+    prices: number
+    date: string
+
+}
+
+interface Film {
+  symbol: string
+  name: string
+  prices: price[]
+}
+
+async function fetchFilms(){
+  const response = await fetch("https://keligmartin.github.io/api/films.json")
+
+  const films: Film[] = await response.json()
+
+  // return liste des films
+  return films
+}
+
+//récupérer les films et les afficher dans la page HTML
+async function displayFilms() {
+
+  //fonction qui récupère les films depuis l’API
+  const films = await fetchFilms()
+  console.log(films)
+
+  }
+
+// Appel de la fonction
+displayFilms()
